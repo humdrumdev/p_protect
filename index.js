5ce05598f8f77c23d36ee932c78f8c62ebd73795
BoringDev <contact@boringdev.me>
2022-05-23 13:39:43 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
-// function to execute program
 const util = require('util');
 
-const exec_program = (prog_name, args) => {
+result = {
+	error: null,
+	stdout: null,
+	stderr: null,
+	size: null,
+};
+
+const exec_program = async (prog_name, args) => {
 	  const exec = util.promisify(require('child_process').exec);
-  return exec(`${prog_name} ${args ? args : ''}`);
+  return await exec(`${prog_name} ${args ? args : ''}`);
 }
-
-const after_exec = (res, err) => {
-	if (err) {
-	console.log(`error: ${err}`);
-	return ;
-}
-  console.log(res.stdout);
-  console.log(`stderr: ${res.stderr}`);
-}
-
-exec_program('echo', "hi").then(after_exec);
 
 const generate_range = (size) => {
 	range = [...Array(size).keys()];
-	cur_indx = size;
-	rand_indx;
+	let cur_indx = size;
+	let rand_indx;
 	while (cur_indx > 0) {
 		rand_indx = Math.floor(Math.random() * cur_indx);
 		cur_indx--;
 		[range[cur_indx], range[rand_indx]] = [range[rand_indx], range[cur_indx]];
 	}
-	return range.join(' ');
+	return "\"" + range.join(' ') + "\"";
 }
 
-const get_program_input = (size) => {
-	let res = {};
-	res.size = size;
+const get_program_input = async (size) => {
+	result.size = size;
+	result.arg = null;
 	if (size)
-		res.arg = generate_range(size);
-	exec_program('./push_swap', res.arg).then(after_exec);
-
+		result.arg = generate_range(size);
+	ret = await exec_program('echo', result.arg);
+	if (ret.error)
+		result.error = err.message;
+	else
+	{
+		result.error = null;
+		result.stdout = ret.stdout;
+		result.stderr = ret.stderr;
+	}
+	return result;
 }
+
+get_program_input(100).then(console.log);
